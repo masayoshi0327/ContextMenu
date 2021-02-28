1e25d58b1a37055896f7259bc1deef68e43a5585
masayoshi0327 <75524615+masayoshi0327@users.noreply.github.com>
2021-02-28 11:32:33 +0900

    Initial Commit

---
+++ ContextMenu/ContextMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContextMenuApp.swift
+//  ContextMenu
+//
+//  Created by 澤崎正佳 on 2021/02/28.
+//
+
+import SwiftUI
+
+@main
+struct ContextMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
