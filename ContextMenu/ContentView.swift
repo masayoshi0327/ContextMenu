295a13fd69b934bb62fa799922e702f5a6824b0f
masayoshi0327 <75524615+masayoshi0327@users.noreply.github.com>
2021-02-28 14:02:47 +0900

    contextMenu 実装

--- ContextMenu/ContentView.swift
+++ ContextMenu/ContentView.swift
@@ -8,9 +8,34 @@
 import SwiftUI
 
 struct ContentView: View {
+    let evs = ["イーブイ", "ブースター", "シャワーズ", "サンダース", "エーフィ", "ブラッキー", "グレイシア", "リーフィア", "ニンフィア"]
+    let colors1: [Color] = [Color(#colorLiteral(red: 0.7254902124, green: 0.4784313738, blue: 0.09803921729, alpha: 1)), .red, .blue, .yellow, Color(#colorLiteral(red: 0.8446564078, green: 0.5145705342, blue: 1, alpha: 0.5)), .black, Color(#colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1)), Color(#colorLiteral(red: 0.5843137503, green: 0.8235294223, blue: 0.4196078479, alpha: 1)), .pink]
+    let colors2: [Color] = [.white, .white, .white, .black, .black, .yellow, .black, .yellow, Color(#colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1))]
+    
+    @State var evID = 0
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack{
+            Circle()
+                .padding()
+                .foregroundColor(colors1[evID])
+            VStack{
+                Text(evs[evID])
+                    .padding()
+                Image(systemName: "sparkles")
+                    .contextMenu(ContextMenu(menuItems: {
+                        ForEach(0..<9) { n in
+                            Button(action: {
+                                evID = n
+                            }, label: {
+                                Text(evs[n])
+                            })
+                        }
+                    }))
+            }
+            .font(.title)
+            .foregroundColor(colors2[evID])
+        }
     }
 }
 
